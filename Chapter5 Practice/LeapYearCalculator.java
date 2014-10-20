1383216c9d5759ff6212bfdb9e3b6ba09e1470cc
Resilienc3 <jsmitty1414@gmail.com>
2014-10-20 11:34:58 -0500

    chpt 5

---
+++ Chapter5 Practice/LeapYearCalculator.java
@@ -0,0 +1,29 @@
+
+
+/**
+ * Write a description of class LeapYearCalculator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LeapYearCalculator
+{
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x+y;
+    }
+
+}
